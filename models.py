473970c14ac973c70f010747760d4bf435421a2e
Milton <milk_candela@hotmail.com>
2025-06-16 15:30:15 -0600

    update and cleaning.

---
+++ models.py
@@ -0,0 +1,141 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import normalize, LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.metrics import r2_score, mean_absolute_error, classification_report
+
+chan = ['Fp1', 'AF3', 'F3', 'F7', 'FC5', 'FC1', 'C3', 'T7',
+        'CP5', 'CP1', 'P3', 'P7', 'PO3', 'O1', 'Oz', 'Pz',
+        'Fp2', 'AF4', 'Fz', 'F4', 'F8', 'FC6', 'FC2', 'Cz',
+        'C4', 'T8', 'CP6', 'CP2', 'P4', 'P8', 'PO4', 'O2']
+schan = ['Fp1', 'Fp2', 'C3', 'C4', 'P7', 'P8', 'O1', 'O2']
+bands = ['Delta', 'Theta', 'Alpha', 'Beta', 'Gamma', 'Eng', 'Fat', 'Exc', 'Rel']
+spec_chan = ['{}_{}'.format(c, s) for c in chan for s in bands]
+spec_schan = ['{}_{}'.format(c, s) for c in schan for s in bands]
+path_pross = 'other/pross/5s_norm/'
+
+def conv_class(x):
+    a = np.empty(x.shape, dtype='<U4')
+    a[x <= 3] = 'Low'
+    a[(x > 3) & (x <= 6)] = 'Med'
+    a[x > 6] = 'Hig'
+    return a
+
+
+with open(path_pross + 'data_training.npy', 'rb') as fileTrain:
+    X = np.load(fileTrain, allow_pickle=True)
+
+with open(path_pross + 'label_training.npy', 'rb') as fileTrainL:
+    Y = np.load(fileTrainL, allow_pickle=True)
+
+def pross_X(x):
+    # Select the 8 OpenBCI channels
+    return pd.DataFrame(x, columns=spec_chan)[spec_schan]
+
+
+# 12 PSD/video * 40 videos/subject * 27 subjects
+# X: (27, 480) # 27 subjects, 480 PSD/subject
+# X: (12960, 160) # 12960 PSDs from all subjects, 288: 32 channels * (5 frequency bands + 4 indices)
+# X = np.stack([np.stack(row) for row in X]).reshape(-1, 288)
+X = np.vstack(X.reshape(-1))
+print(X.shape)
+X = pross_X(X)
+
+# Y = np.stack([np.stack(row) for row in Y]).reshape(-1, 4)
+Y = np.vstack(Y.reshape(-1))
+print(Y.shape)
+# Z = np.ravel(Y[:, 1])
+Z = conv_class(Y)
+
+print(X)
+print(Z)
+
+# model = RandomForestClassifier(random_state=1, n_jobs=1).fit(X, Z[:, 0])
+# model.fit(X, Z[:, 0])
+# print(classification_report(model.predict(X), Z[:, 0]))
+
+Arousal_Train = np.ravel(Y[:, 0])
+Valence_Train = np.ravel(Y[:, 1])
+Domain_Train = np.ravel(Y[:, 2])
+
+# Aimp = RandomForestRegressor(random_state=1, n_jobs=1).fit(X, Arousal_Train).feature_importances_
+# Vimp = RandomForestRegressor(random_state=1, n_jobs=1).fit(X, Valence_Train).feature_importances_
+# Dimp = RandomForestRegressor(random_state=1, n_jobs=1).fit(X, Domain_Train).feature_importances_
+
+# tindices = np.argsort((Aimp + Vimp + Dimp)/3)[::-1][:20]
+# X = X.iloc[:, tindices]
+# print(X.shape)
+
+with open(path_pross + 'data_testing.npy', 'rb') as fileTrain:
+    M = np.load(fileTrain, allow_pickle=True)
+
+with open(path_pross + 'label_testing.npy', 'rb') as fileTrainL:
+    N = np.load(fileTrainL, allow_pickle=True)
+
+# M = pross_X(normalize(M))
+# M = np.stack([np.stack(row) for row in M]).reshape(-1, 288)
+M = np.vstack(M.reshape(-1))
+M = pross_X(M)
+# M = M.iloc[:, tindices]
+print(M.shape)
+
+# N = np.stack([np.stack(row) for row in N]).reshape(-1, 4)
+N = np.vstack(N.reshape(-1))
+L = np.ravel(N[:, 1])
+
+Arousal_Test = np.ravel(N[:, 0])
+Valence_Test = np.ravel(N[:, 1])
+Domain_Test = np.ravel(N[:, 2])
+
+from pickle import dump
+
+# Entrenamiento de Regresores
+print('Entrenando modelo Valence...')
+Val_R = RandomForestRegressor(random_state=1)
+#Val_R = LinearRegression()
+# print(X[0:468480:16].shape)
+# Val_R.fit(X[0:468480:16], Valence_Train[0:468480:16])
+Val_R.fit(X, Valence_Train)
+with open('reg_val_model2.pkl', 'wb') as file:
+    dump(Val_R, file)
+print('Modelo Valence entrenado!')
+
+print('Entrenando modelo Arousal...')
+Aro_R = RandomForestRegressor(random_state=1)
+#Aro_R = LinearRegression()
+# Aro_R.fit(X[0:468480:16], Arousal_Train[0:468480:16])
+Aro_R.fit(X, Arousal_Train)
+with open('reg_aro_model2.pkl', 'wb') as file:
+    dump(Aro_R, file)
+print('Modelo Arousal entrenado!')
+
+print('Entrenando modelo Dominance...')
+# Dom_R = RandomForestRegressor(n_estimators=512, n_jobs=6)
+Dom_R = RandomForestRegressor(random_state=1)
+#Dom_R = LinearRegression()
+# Dom_R.fit(X[0:468480:16], Domain_Train[0:468480:16])
+Dom_R.fit(X, Domain_Train)
+with open('reg_dom_model2.pkl', 'wb') as file:
+    dump(Dom_R, file)
+print('Modelo Dominance entrenado!')
+
+val_pred = Val_R.predict(M)
+aro_pred = Aro_R.predict(M)
+dom_pred = Dom_R.predict(M)
+
+# r2_score, mean_absolute_error, mean_squared_error
+# r2_val = r2_score(Valence_Test[0:74240:512], val_pred)
+r2_val = r2_score(Valence_Test, val_pred)
+print("R-squared Score Valence:", r2_val)
+r2_aro = r2_score(Arousal_Test, aro_pred)
+print("R-squared Score Arousal:", r2_aro)
+r2_dom = r2_score(Domain_Test, dom_pred)
+print("R-squared Score Dominance:", r2_dom)
+
+mae_val = mean_absolute_error(Valence_Test, val_pred)
+print("Mean Absolute Error Valence:", mae_val)
+mae_aro = mean_absolute_error(Arousal_Test, aro_pred)
+print("Mean Absolute Error Arousal:", mae_aro)
+mae_dom = mean_absolute_error(Domain_Test, dom_pred)
+print("Mean Absolute Error Dominance:", mae_dom)
---
+++ prep_data.py
@@ -0,0 +1,144 @@
+import numpy as np
+import pickle as pickle
+import os
+from brainflow.data_filter import DataFilter, WindowOperations
+import pyeeg as pe
+
+# Configuración para la función np.load para cargar archivos.
+# Los valores por defecto establecidos incluyen el uso de codificación ASCII.
+np.load.__defaults__=(None, True, True, 'ASCII')
+
+folder = 'data/pross/10s_norm'
+# Definición de los canales EEG que se desean utilizar (8 canales de openbci).
+channel = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+
+# Frecuencias de las bandas para calcular el espectro de potencia (PSD) de la señal EEG.
+band = [.5,4,8,12,30,45]  # Definición de 5 bandas.
+
+# Tamaño de ventana para promediar la potencia en bandas durante 5 segundos (downsampleado a 128 Hz).
+window_size = 128 * 10  # Ventana de 5 segundos a una frecuencia de muestreo de 128 Hz.
+
+# Frecuencia de muestreo de la señal EEG.
+fs = 128  # Frecuencia de muestreo de 128 Hz.
+
+# Lista de sujetos para cargar los datos.
+subjectList = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32']
+
+
+def brainflow_bandpowers(signal, fs, bands):
+    # Compute PSD using Welch
+    psd = DataFilter.get_psd_welch(signal.astype(np.float64), nfft=fs, overlap=fs//2,
+                                   sampling_rate=fs, window=WindowOperations.BLACKMAN_HARRIS)
+    # print(psd)
+    # Compute band power for each adjacent pair in bands
+    band_powers = []
+    for i in range(len(bands)-1):
+        low, high = bands[i], bands[i+1]
+        power = DataFilter.get_band_power(psd, low, high)
+        band_powers.append(power)
+    return band_powers
+
+
+def add_index(psd):
+    #          0      1      2     3      4
+    # PSD = [delta, theta, alpha, beta, gamma]
+    psd.append(psd[3]/(psd[2]+psd[1]))  # Engagement (beta/(alpha+theta))
+    psd.append(psd[2]/psd[1])           # Fatigue (alpha/theta)
+    psd.append(psd[3]/psd[2])           # Excitement (beta/alpha)
+    psd.append(psd[1]/psd[0])           # Relaxation (theta/delta)
+    return psd
+
+
+# Definición de la función para procesar datos EEG
+def FFT_Processing(sub, channel, band, window_size, fs):
+    # Lista para almacenar metadatos
+    meta = []
+
+    # Abrir los datos usando pickle para descomprimir
+    with open('data/raw/s' + sub + '.dat', 'rb') as file:
+        subject = pickle.load(file, encoding='latin1')  # Resuelve el problema de datos de Python 2 utilizando la codificación latin1
+
+        for i in range(0, 40):
+            # Para las 40 pruebas realizadas
+            data = subject["data"][i]
+            labels = subject["labels"][i]
+            start = fs*3-1
+
+            # Baseline-normalized band power
+            cal_psd = np.zeros((len(channel), len(band)-1 + 4))
+            for j in range(len(channel)):
+                cal_psd[j] = add_index(psd=brainflow_bandpowers(data[j][:(fs*3)], fs, band))
+
+            while start + window_size < data.shape[1]:
+                meta_array, meta_data = [], []
+                for j in range(len(channel)):
+                    X = data[j][start:(start+window_size)]  # Dividir los datos crudos en ventanas de 2 segundos, con un intervalo de 0.125 segundos
+                    # Y = pe.bin_power(X, band, fs)  # FFT en 5 segundos del canal j, en secuencia de theta, alpha, beta baja, beta alta, gamma
+                    # meta_data = meta_data + list(Y[0])
+                    # PyEEG: 524.5068,  3251.6598,  3527.7513, 10472.2545, 2828.3445
+                    # Brainflow: 0.1634, 1.1050, 1.3249, 2.0719, 0.1612
+                    Y = add_index(psd=brainflow_bandpowers(X, fs, band))
+
+                    # Normalization according to: (x - mean(cal))/mean(cal)
+                    for z in range(len(band)-1 + 4):
+                        Y[z] = (Y[z] - cal_psd[j, z])/cal_psd[j, z]
+
+                    meta_data += Y
+
+                meta_array.append(np.array(meta_data))
+                meta_array.append(labels)
+
+                meta.append(np.array(meta_array))
+                start += window_size
+        meta = np.array(meta)
+        np.save(folder + '/s' + sub, meta, allow_pickle=True, fix_imports=True)
+
+for subjects in subjectList:
+    FFT_Processing (subjects, channel, band, window_size, fs)
+
+# Listas para almacenar datos y etiquetas de los diferentes conjuntos
+data_training, label_training = [], []
+data_testing, label_testing = [], []
+data_validation, label_validation = [], []
+
+# Iteramos sobre la lista de sujetos
+for subject in subjectList:
+    # Cargamos los datos procesados para el sujeto actual
+    with open(os.path.join(folder, f's{subject}.npy'), 'rb') as file:
+        sub = np.load(file)
+        # Iteramos sobre los datos procesados del sujeto
+        if int(subject) <= 30:
+            data_training.append(sub[:, 0])
+            label_training.append(sub[:, 1])
+        elif int(subject) > 30:
+            data_testing.append(sub[:, 0])
+            label_testing.append(sub[:, 1])
+
+        '''
+        for i in range(sub.shape[0]):
+            if i % 8 == 0:
+                data_testing.append(sub[i][0])
+                label_testing.append(sub[i][1])
+            elif i % 8 == 1:
+                data_validation.append(sub[i][0])
+                label_validation.append(sub[i][1])
+            else:
+                data_training.append(sub[i][0])
+                label_training.append(sub[i][1])
+        '''
+
+# Guardamos los conjuntos de datos y etiquetas en archivos
+np.save(os.path.join(folder, 'data_training.npy'), np.array(data_training), allow_pickle=True, fix_imports=True)
+np.save(os.path.join(folder, 'label_training.npy'), np.array(label_training), allow_pickle=True, fix_imports=True)
+np.save(os.path.join(folder, 'data_testing.npy'), np.array(data_testing), allow_pickle=True, fix_imports=True)
+np.save(os.path.join(folder, 'label_testing.npy'), np.array(label_testing), allow_pickle=True, fix_imports=True)
+# np.save(os.path.join('other/pross/5s_norm', 'data_validation.npy'), np.array(data_validation), allow_pickle=True, fix_imports=True)
+# np.save(os.path.join('other/pross/5s_norm', 'label_validation.npy'), np.array(label_validation), allow_pickle=True, fix_imports=True)
+
+# Imprimimos las dimensiones de los conjuntos de datos y etiquetas para cada conjunto
+print("Training dataset:", np.array(data_training).shape, np.array(label_training).shape)
+print("Testing dataset:", np.array(data_testing).shape, np.array(label_testing).shape)
+print("Validation dataset:", np.array(data_validation).shape, np.array(label_validation).shape)
+
+# Restauramos los valores por defecto para la función np.load
+np.load.__defaults__=(None, False, True, 'ASCII')
